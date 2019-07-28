72d9601ad79c838ec2b959eb80e02eced2fd47df
Daniel <ardekanicapital@gmail.com>
2019-07-27 20:55:57 -0400

    Create 'get' and 'post' routes, workout controller, index and create functions, and correlating views

--- app/controllers/api/workout_controller.rb
+++ app/controllers/api/workout_controller.rb
@@ -1,2 +1,19 @@
 class Api::WorkoutController < ApplicationController
+
+  def index
+    @workout = Workout.all
+    render 'index.json.jb'
+  end
+
+  def create
+    @workout = Workout.new(
+    name: params[:name],
+    description: params[:description],
+    muscle_group: params[:muscle_group]
+    )
+
+    @workout.save
+    render "create.json.jb"
+  end
 end
+  
